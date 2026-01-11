1d3bfaa234fd620abbef7fe02af7a798f68f91ca
simonp <simon-kirchzarten@web.de>
2026-01-11 12:02:49 +0100

    Added Aldi Cover Image

--- src/hooks/useAldi.js
+++ src/hooks/useAldi.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
+import aldiCover from '../assets/Aldi.jpg';
 
 // Hardcoded
 const PROSPEKT_URL = "https://prospekt.aldi-sued.de/kw03-26-op-mp/page/1";
-const COVER_IMAGE = "https://view.publitas.com/56570/2034870/pages/225c9602-0e26-4074-be6a-93118ce245f7-at1600.jpg";
+const COVER_IMAGE = aldiCover;
 
 export function useAldi() {
     return {
