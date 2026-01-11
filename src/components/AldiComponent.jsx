f7afaa93ca213dfdea9e6d752388a3aa7dee20d8
simonp <simon-kirchzarten@web.de>
2026-01-11 12:05:33 +0100

    fixed size

--- src/components/AldiComponent.jsx
+++ src/components/AldiComponent.jsx
@@ -21,7 +21,7 @@ export function AldiComponent() {
             </div>
 
             {/* Main Image (Contained for visibility) */}
-            <div className="absolute inset-0 flex items-center justify-center p-4">
+            <div className="absolute inset-0 flex items-center justify-center p-16">
                 <img
                     src={coverImage}
                     alt="Aldi Prospekt Cover"
